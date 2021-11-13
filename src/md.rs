d017aaf0dd97bfe70a5d8d49d0d6202b9848bcd4
zjp <jiping_zhou@foxmail.com>
2021-11-14 01:05:23 +0800

    #15 重构 Md：完成段落分批计算部分

--- src/md.rs
+++ src/md.rs
@@ -5,11 +5,12 @@ use pulldown_cmark::{
     Tag::*,
 };
 use pulldown_cmark_to_cmark::Options as OutOptions;
+use std::mem::replace;
 
 #[derive(Debug)]
 pub struct Md<'e> {
     /// 解析 md 文件的事件
-    events: Vec<Event<'e>>,
+    events: Box<[Event<'e>]>,
     /// 内部缓冲。有两个用途：
     /// 1. 提取的原文段落；
     /// 2. 原文填充翻译内容之后的 md 文本。
@@ -18,7 +19,8 @@ pub struct Md<'e> {
     /// 大于 1024B 的文本以原文 2 倍字节长度初始化。
     buffer: String,
     /// output 的 bytes 分布
-    para:   Vec<usize>,
+    bytes:  Vec<usize>,
+    chars:  Vec<usize>,
     limit:  Limit,
 }
 
@@ -32,11 +34,12 @@ impl<'e> Md<'e> {
                        if capacity < MINIMUM_CAPACITY { MINIMUM_CAPACITY } else { capacity * 2 };
                    String::with_capacity(capacity)
                },
-               para:   Vec::with_capacity(128), // 预先分配 128 个段落
+               bytes:  Vec::with_capacity(128), // 预先分配 128 个段落
+               chars:  Vec::with_capacity(128), // 预先分配 128 个段落
                limit:  Limit::default(), }
     }
 
-    /// 提取文本
+    /// 提取原文的段落文本。
     ///
     /// TODO: 尽可能保存原样式/结构
     pub fn extract(&mut self) -> &str {
@@ -47,11 +50,12 @@ impl<'e> Md<'e> {
         &self.buffer
     }
 
-    /// 提取文本，并以字节单位记录段落分布。
+    /// 提取原文的段落文本，并以字节为单位记录段落分布。
     /// # 注意
     /// - 本方法返回提取后的所有文本，而且本方法一般只需要调用一次。
-    /// - 需要分批段落，请调用：[`bytes_paragraph`][`Md::bytes_paragraph`]。
-    /// - 需要分批段落的字节长度或范围，请调用：[`bytes_range`][`Md::bytes_range`]。
+    /// - 本方法比 [`extract`][`Md::extract`] 多做了一件事：计算和记录每个段落的字节长度。
+    /// - 需要每个段落的字节长度或字节范围，请调用：[`bytes_range`][`Md::bytes_range`]。
+    /// - 需要对段落按字节上限分批，请调用：[`bytes_paragraph`][`Md::bytes_paragraph`]。
     ///
     /// TODO: 尽可能保存原样式/结构
     pub fn extract_with_bytes(&mut self) -> &str {
@@ -59,40 +63,109 @@ impl<'e> Md<'e> {
         self.para_clear();
         let mut select = true;
         let buf = &mut self.buffer;
-        let mut len = 0;
-        let vec = &mut self.para;
+        let len = &mut 0;
+        let bytes = &mut self.bytes;
         self.events
             .iter()
-            .for_each(|event| extract_with_bytes(event, &mut select, buf, &mut len, vec));
+            .for_each(|event| extract_with_bytes(event, &mut select, buf, len, bytes));
+        &self.buffer
+    }
+
+    /// 提取原文的段落文本，并以字符为单位记录段落分布。
+    /// # 注意
+    /// - 本方法返回提取后的所有文本，而且本方法一般只需要调用一次。
+    /// - 本方法比 [`extract_with_bytes`][`Md::extract_with_bytes`]
+    ///   多做了一件事：计算和记录每个段落的字符长度。 所以
+    ///   [`bytes_paragraph`][`Md::bytes_paragraph`] 和 [`bytes_range`][`Md::bytes_range`]
+    ///   方法均可调用。
+    /// - 需要每个段落的字符长度或字节范围，请调用：[`chars_range`][`Md::chars_range`]。
+    /// - 需要对段落按字符上限分批，请调用：[`chars_paragraph`][`Md::chars_paragraph`]。
+    ///
+    /// TODO: 尽可能保存原样式/结构
+    pub fn extract_with_chars(&mut self) -> &str {
+        self.buffer_clear();
+        self.para_clear();
+        let mut select = true;
+        let buf = &mut self.buffer;
+        let len = &mut 0;
+        let cnt = &mut 0;
+        let bytes = &mut self.bytes;
+        let chars = &mut self.chars;
+        self.events
+            .iter()
+            .for_each(|event| extract_with_chars(event, &mut select, buf, len, bytes, cnt, chars));
         &self.buffer
     }
 
     /// 以字节数量分割段落批次。
+    /// 分批策略如下：
+    /// - 当返回 `Some` 时，意味着返回完整的段落，且至少返回一个段落；
+    /// - 当返回 `None` 时，意味着已经返回所有段落。
+    /// - 每次返回的段落有两种情况：
+    ///
+    ///   1. 不多于 limit 字节大小的完整段落（至少一个完整段落）；
+    ///   2. 字节大小超过 limit 的**一个**段落。
     ///
     /// ## 注意
-    /// - 请先调用一次 [`extract_with_bytes`][`Md::extract_with_bytes`] 再调用此方法。
+    /// - 请先调用一次 [`extract_with_bytes`][`Md::extract_with_bytes`] 或者
+    ///   [`extract_with_chars`][`Md::extract_with_chars`] 再调用此方法。
     /// - 此方法可以多次调用：这在需要不同 limit 的分批时很有用。
     pub fn bytes_paragraph(&mut self, limit: usize) -> impl Iterator<Item = &str> {
         self.limit = Limit::new(limit);
         let limit = &mut self.limit;
-        let f = |l: &usize| if let Some(i) = limit.batch(*l) { self.buffer.get(i) } else { None };
-        self.para.iter().chain(std::iter::once(&usize::MAX)).filter_map(f)
+        let f = |l: &usize| if let Some(i) = limit.bytes(*l) { self.buffer.get(i) } else { None };
+        self.bytes.iter().chain(std::iter::once(&usize::MAX)).filter_map(f)
     }
 
-    /// 返回每个分批段落的字节数和范围。
+    /// 以字符数量分割段落批次。
+    /// 分批策略如下：
+    /// - 当返回 `Some` 时，意味着返回完整的段落，且至少返回一个段落；
+    /// - 当返回 `None` 时，意味着已经返回所有段落。
+    /// - 每次返回的段落有两种情况：
+    ///
+    ///   1. 不多于 limit 字节大小的完整段落（至少一个完整段落）；
+    ///   2. 字节大小超过 limit 的**一个**段落。
     ///
     /// ## 注意
-    /// - 请先调用一次 [`extract_with_bytes`][`Md::extract_with_bytes`] 再调用此方法。
-    /// - 此方法可以多次调用。
-    #[rustfmt::skip]
-    pub fn bytes_range<'r>(&'r self) -> impl Iterator<Item = (usize, Range)> + 'r {
-        self.para.iter().scan(0, |state, &l| Some((l, std::mem::replace(state, *state+l)..*state)))
+    /// - 请先调用一次 [`extract_with_chars`][`Md::extract_with_chars`] 再调用此方法。
+    /// - 此方法可以多次调用：这在需要不同 limit 的分批时很有用。
+    pub fn chars_paragraph(&mut self, limit: usize) -> impl Iterator<Item = &str> {
+        self.limit = Limit::new(limit);
+        let limit = &mut self.limit;
+        let f = |(c, l): (&usize, &usize)| {
+            if let Some(i) = limit.chars(*c, *l) {
+                self.buffer.get(i)
+            } else {
+                None
+            }
+        };
+        let iter = std::iter::once(&usize::MAX);
+        self.chars
+            .iter()
+            .chain(iter.clone())
+            .zip(self.bytes.iter().chain(iter))
+            .filter_map(f)
     }
 
-    /// 完成并返回写入翻译内容。参数 `paragraph` 为按段落翻译的译文。
+    /// 提取的每个原文段落的字节数。
+    pub fn bytes<'r>(&'r self) -> impl Iterator<Item = usize> + 'r { self.bytes.iter().copied() }
+
+    /// 提取的每个原文段落的字符数。
+    pub fn chars<'r>(&'r self) -> impl Iterator<Item = usize> + 'r { self.chars.iter().copied() }
+
+    /// 提取的每个原文段落的字符数、字节数和字节范围。
+    pub fn chars_bytes_range<'r>(&'r self) -> impl Iterator<Item = (usize, usize, Range)> + 'r {
+        self.chars()
+            .zip(self.bytes())
+            .scan(0, |state, (c, l)| Some((c, l, replace(state, *state + l)..*state)))
+    }
+
+    /// 完成并返回写入翻译内容。参数 `paragraph` 为按段落翻译的**译文**。
     pub fn done(mut self, mut paragraph: impl Iterator<Item = &'e str>) -> String {
         self.buffer_clear(); // 清除段落缓冲
-        let output = self.events.into_iter().map(|event| prepend(event, &mut paragraph)).flatten();
+                             // let events: Vec<_> = self.events.into();
+        let output =
+            self.events.into_vec().into_iter().map(|e| prepend(e, &mut paragraph)).flatten();
         let opt = cmark_to_cmark_opt();
         pulldown_cmark_to_cmark::cmark_with_options(output, &mut self.buffer, None, opt).unwrap();
         // dbg!(self.output.len(),
@@ -111,8 +184,8 @@ impl<'e> Md<'e> {
     }
 
     fn para_clear(&mut self) {
-        if !self.para.is_empty() {
-            self.para.clear()
+        if !self.bytes.is_empty() {
+            self.bytes.clear()
         }
     }
 }
@@ -122,27 +195,67 @@ type Range = std::ops::Range<usize>;
 #[derive(Debug, Default)]
 struct Limit {
     limit: usize,
-    bat:   usize,
+    cnt:   usize,
+    len:   usize,
     pos:   usize,
 }
 
 impl Limit {
     #[rustfmt::skip]
-    fn new(limit: usize) -> Self { Self { limit, bat: 0, pos: 0 } }
+    fn new(limit: usize) -> Self { Self { limit, cnt: 0, len: 0, pos: 0 } }
 
-    fn batch(&mut self, len: usize) -> Option<Range> {
-        if let Some(add) = self.bat.checked_add(len) {
-            if add < self.limit {
-                self.bat += len;
-                None
+    fn bytes(&mut self, len: usize) -> Option<Range> {
+        if let Some(add) = self.len.checked_add(len) {
+            if add <= self.limit {
+                self.len = add;
+                return None;
             } else {
                 let p = self.pos;
-                self.pos += if self.bat == 0 { len } else { std::mem::replace(&mut self.bat, len) };
-                Some(p..self.pos)
+                let rhs = if self.len == 0 { len } else { replace(&mut self.len, len) };
+                // 到达最后一批时：当 bat=0, len=usize::Max 时会出现 Err
+                if let Some(add) = self.pos.checked_add(rhs) {
+                    self.pos = add;
+                    return Some(p..self.pos);
+                }
             }
+        }
+        // 返回最后一批段落
+        if self.len == 0 {
+            // 当 len=0 时，最后一批段落是空串，因此需要提前结束掉
+            None
         } else {
-            // 返回最后一个段落
-            Some(self.pos..self.pos + self.bat)
+            Some(self.pos..self.pos + self.len)
+        }
+    }
+
+    fn chars(&mut self, cnt: usize, len: usize) -> Option<Range> {
+        dbg!(len, &self);
+        if let Some(add) = self.len.checked_add(len) {
+            if self.cnt + cnt <= self.limit {
+                self.len = add;
+                self.cnt += cnt;
+                return None;
+            } else {
+                let p = self.pos;
+                let rhs = if self.len == 0 {
+                    len
+                } else {
+                    self.cnt = cnt;
+                    replace(&mut self.len, len)
+                };
+                // 到达最后一批时：当 bat=0, len=usize::Max 时会出现 Err
+                if let Some(add) = self.pos.checked_add(rhs) {
+                    self.pos = add;
+                    return Some(p..self.pos);
+                }
+            }
+        }
+        // 返回最后一批段落
+        if self.len == 0 {
+            // 当 len=0 时，最后一批段落是空串，因此需要提前结束掉
+            None
+        } else {
+            Some(self.pos..self.pos + self.len)
         }
     }
 }
@@ -192,7 +305,7 @@ pub fn extract(event: &Event, select: &mut bool, buf: &mut String) {
     }
 }
 
-/// 取出需要被翻译的内容：按照段落或标题
+/// 取出需要被翻译的内容：按照段落或标题。
 pub fn extract_with_bytes(event: &Event, select: &mut bool, buf: &mut String, len: &mut usize,
                           vec: &mut Vec<usize>) {
     match event {
@@ -214,6 +327,40 @@ pub fn extract_with_bytes(event: &Event, select: &mut bool, buf: &mut String, le
             buf.push_str(x.as_ref());
             buf.push('`');
             *len += x.len() + 2;
+        }
+        Start(CodeBlock(_)) => *select = false,
+        End(CodeBlock(_)) => *select = true,
+        _ => (),
+    }
+}
+
+/// 取出需要被翻译的内容：按照段落或标题
+pub fn extract_with_chars(event: &Event, select: &mut bool, buf: &mut String, len: &mut usize,
+                          bytes: &mut Vec<usize>, cnt: &mut usize, chars: &mut Vec<usize>) {
+    match event {
+        End(Paragraph | Heading(_)) => {
+            buf.push('\n');
+            bytes.push(*len + 1);
+            chars.push(*cnt + 1);
+            *len = 0;
+            *cnt = 0;
+        }
+        Text(x) if *select => {
+            buf.push_str(x.as_ref());
+            *len += x.len();
+            *cnt += x.chars().count();
+        }
+        SoftBreak | HardBreak => {
+            buf.push(' ');
+            *len += 1;
+            *cnt += 1;
+        }
+        Code(x) => {
+            buf.push('`');
+            buf.push_str(x.as_ref());
+            buf.push('`');
+            *len += x.len() + 2;
+            *cnt += x.chars().count() + 2;
         }
         Start(CodeBlock(_)) => *select = false,
         End(CodeBlock(_)) => *select = true,
--- tests/extract.rs
+++ tests/extract.rs
@@ -188,9 +188,14 @@ code block
 #[test]
 fn size() {
     use std::mem::size_of;
-    assert_debug_snapshot!(size_of::<Option<Event>>(), @"64");
+    assert_debug_snapshot!(size_of::<Option<Event>>(),  @"64");
     assert_debug_snapshot!(size_of::<Option<CowStr>>(), @"24");
-    assert_debug_snapshot!(size_of::<Option<Tag>>(), @"56");
+    assert_debug_snapshot!(size_of::<Option<Tag>>(),    @"56");
+
+    assert_debug_snapshot!(size_of::<Md>(),             @"120");
+    assert_debug_snapshot!(size_of::<Vec<usize>>(),     @"24");
+    assert_debug_snapshot!(size_of::<String>(),         @"24");
+    assert_debug_snapshot!(size_of::<Box<[usize]>>(),   @"16");
 }
 
 #[rustfmt::skip]
@@ -212,7 +217,7 @@ fn process_epoll_events(&mut self, event_id: usize) {
 
 
 Hi!
-
+你好！这里是中文！
 
 
 
@@ -228,66 +233,137 @@ Hi!
 ";
 
 #[test]
-fn bytes_next_paragraph_test() {
+fn batch_paragraph() {
     let mut md = Md::new(MD);
-    assert_display_snapshot!(md.extract_with_bytes().len(), @"826"); // 段落文本总长度
-    assert_debug_snapshot!(md.bytes_range().collect::<Vec<_>>(), @r###"
+    assert_display_snapshot!(md.extract_with_chars().len(), @"854"); // 段落文本总字节数
+    assert_debug_snapshot!(md.chars_bytes_range().collect::<Vec<_>>(), @r###"
     [
         (
+            16,
             16,
             0..16,
         ),
         (
             317,
+            317,
             16..333,
         ),
         (
-            4,
-            333..337,
+            14,
+            32,
+            333..365,
         ),
         (
             305,
-            337..642,
+            305,
+            365..670,
         ),
         (
             184,
-            642..826,
+            184,
+            670..854,
         ),
     ]
     "###);
-
+    assert_debug_snapshot!(md.bytes_paragraph(1 << 10).collect::<Vec<_>>(), @r###"
+    [
+        "I/O event queue\nWe add the `callback_id` to the collection of callbacks to run. We pass in `Js::Undefined` since we'll not actually pass any data along here. You'll see why when we reach the Http module chapter, but the main point is that the I/O queue doesn't return any data itself, it just tells us that data is ready to be read.\nHi! 你好！这里是中文！\nHi! Why even keep track of how many `epoll_events` are pending? We don't use this value here, but I added it to make it easier to create some `print` statements showing the status of our runtime at different points. However, there are good reasons to keep track of these events even if we don't use them.\nOne area we're taking shortcuts on all the way here is security. If someone were to build a public facing server out of this, we need to account for slow networks and malicious users.\n",
+    ]
+    "###);
     assert_debug_snapshot!(md.bytes_paragraph(400).collect::<Vec<_>>(), @r###"
     [
-        "I/O event queue\nWe add the `callback_id` to the collection of callbacks to run. We pass in `Js::Undefined` since we'll not actually pass any data along here. You'll see why when we reach the Http module chapter, but the main point is that the I/O queue doesn't return any data itself, it just tells us that data is ready to be read.\nHi!\n",
+        "I/O event queue\nWe add the `callback_id` to the collection of callbacks to run. We pass in `Js::Undefined` since we'll not actually pass any data along here. You'll see why when we reach the Http module chapter, but the main point is that the I/O queue doesn't return any data itself, it just tells us that data is ready to be read.\nHi! 你好！这里是中文！\n",
+        "Hi! Why even keep track of how many `epoll_events` are pending? We don't use this value here, but I added it to make it easier to create some `print` statements showing the status of our runtime at different points. However, there are good reasons to keep track of these events even if we don't use them.\n",
+        "One area we're taking shortcuts on all the way here is security. If someone were to build a public facing server out of this, we need to account for slow networks and malicious users.\n",
+    ]
+    "###);
+    assert_debug_snapshot!(md.bytes_paragraph(16).collect::<Vec<_>>(), @r###"
+    [
+        "I/O event queue\n",
+        "We add the `callback_id` to the collection of callbacks to run. We pass in `Js::Undefined` since we'll not actually pass any data along here. You'll see why when we reach the Http module chapter, but the main point is that the I/O queue doesn't return any data itself, it just tells us that data is ready to be read.\n",
+        "Hi! 你好！这里是中文！\n",
         "Hi! Why even keep track of how many `epoll_events` are pending? We don't use this value here, but I added it to make it easier to create some `print` statements showing the status of our runtime at different points. However, there are good reasons to keep track of these events even if we don't use them.\n",
         "One area we're taking shortcuts on all the way here is security. If someone were to build a public facing server out of this, we need to account for slow networks and malicious users.\n",
     ]
     "###);
 
-    assert_debug_snapshot!(md.bytes_paragraph(1 << 10).collect::<Vec<_>>(), @r###"
+    assert_display_snapshot!(md.chars().sum::<usize>(), @"836"); // 段落文本总字符数
+    assert_debug_snapshot!(md.chars_paragraph(347).collect::<Vec<_>>(), @r###"
     [
-        "I/O event queue\nWe add the `callback_id` to the collection of callbacks to run. We pass in `Js::Undefined` since we'll not actually pass any data along here. You'll see why when we reach the Http module chapter, but the main point is that the I/O queue doesn't return any data itself, it just tells us that data is ready to be read.\nHi!\nHi! Why even keep track of how many `epoll_events` are pending? We don't use this value here, but I added it to make it easier to create some `print` statements showing the status of our runtime at different points. However, there are good reasons to keep track of these events even if we don't use them.\nOne area we're taking shortcuts on all the way here is security. If someone were to build a public facing server out of this, we need to account for slow networks and malicious users.\n",
+        "I/O event queue\nWe add the `callback_id` to the collection of callbacks to run. We pass in `Js::Undefined` since we'll not actually pass any data along here. You'll see why when we reach the Http module chapter, but the main point is that the I/O queue doesn't return any data itself, it just tells us that data is ready to be read.\nHi! 你好！这里是中文！\n",
+        "Hi! Why even keep track of how many `epoll_events` are pending? We don't use this value here, but I added it to make it easier to create some `print` statements showing the status of our runtime at different points. However, there are good reasons to keep track of these events even if we don't use them.\n",
+        "One area we're taking shortcuts on all the way here is security. If someone were to build a public facing server out of this, we need to account for slow networks and malicious users.\n",
+    ]
+    "###);
+
+    // 极端情况
+    assert_debug_snapshot!(md.bytes_paragraph(0).collect::<Vec<_>>(), @r###"
+    [
+        "I/O event queue\n",
+        "We add the `callback_id` to the collection of callbacks to run. We pass in `Js::Undefined` since we'll not actually pass any data along here. You'll see why when we reach the Http module chapter, but the main point is that the I/O queue doesn't return any data itself, it just tells us that data is ready to be read.\n",
+        "Hi! 你好！这里是中文！\n",
+        "Hi! Why even keep track of how many `epoll_events` are pending? We don't use this value here, but I added it to make it easier to create some `print` statements showing the status of our runtime at different points. However, there are good reasons to keep track of these events even if we don't use them.\n",
+        "One area we're taking shortcuts on all the way here is security. If someone were to build a public facing server out of this, we need to account for slow networks and malicious users.\n",
+    ]
+    "###);
+    assert_debug_snapshot!(md.chars_paragraph(0).collect::<Vec<_>>(), @r###"
+    [
+        "I/O event queue\n",
+        "We add the `callback_id` to the collection of callbacks to run. We pass in `Js::Undefined` since we'll not actually pass any data along here. You'll see why when we reach the Http module chapter, but the main point is that the I/O queue doesn't return any data itself, it just tells us that data is ready to be read.\n",
+        "Hi! 你好！这里是中文！\n",
+        "Hi! Why even keep track of how many `epoll_events` are pending? We don't use this value here, but I added it to make it easier to create some `print` statements showing the status of our runtime at different points. However, there are good reasons to keep track of these events even if we don't use them.\n",
+        "One area we're taking shortcuts on all the way here is security. If someone were to build a public facing server out of this, we need to account for slow networks and malicious users.\n",
     ]
     "###);
 }
 
 #[test]
-fn md_limit_test() {
+fn md_and_range() {
     let mut md = Md::new(MD);
-    let buf = md.extract_with_bytes().to_owned();
+    let buf = md.extract_with_chars().to_owned();
     {
-        assert_debug_snapshot!(md.bytes_range().next().unwrap().1, @"0..16");
+        assert_debug_snapshot!(md.chars_bytes_range().next().unwrap(), @r###"
+        (
+            16,
+            16,
+            0..16,
+        )
+        "###);
     }
     {
-        assert_debug_snapshot!(md.bytes_range().next().unwrap().1, @"0..16");
-        assert_debug_snapshot!(md.bytes_range().next().unwrap().1, @"0..16");
+        assert_debug_snapshot!(md.chars_bytes_range().next().unwrap(), @r###"
+        (
+            16,
+            16,
+            0..16,
+        )
+        "###);
+        assert_debug_snapshot!(md.chars_bytes_range().next().unwrap(), @r###"
+        (
+            16,
+            16,
+            0..16,
+        )
+        "###);
     }
     {
-        let mut range = md.bytes_range();
-        assert_debug_snapshot!(range.next().unwrap().1, @"0..16");
-        assert_debug_snapshot!(range.next().unwrap().1, @"16..333");
+        let mut range = md.chars_bytes_range();
+        assert_debug_snapshot!(range.next().unwrap(), @r###"
+        (
+            16,
+            16,
+            0..16,
+        )
+        "###);
+        assert_debug_snapshot!(range.next().unwrap(), @r###"
+        (
+            317,
+            317,
+            16..333,
+        )
+        "###);
     }
-    assert_debug_snapshot!(md.bytes_range().map(|(l, i)| (l, &buf[i])).collect::<Vec<_>>(),
+    assert_debug_snapshot!(md.chars_bytes_range().map(|(_, _, i)| (i.len(), &buf[i])).collect::<Vec<_>>(),
     @r###"
     [
         (
@@ -299,8 +375,8 @@ fn md_limit_test() {
             "We add the `callback_id` to the collection of callbacks to run. We pass in `Js::Undefined` since we'll not actually pass any data along here. You'll see why when we reach the Http module chapter, but the main point is that the I/O queue doesn't return any data itself, it just tells us that data is ready to be read.\n",
         ),
         (
-            4,
-            "Hi!\n",
+            32,
+            "Hi! 你好！这里是中文！\n",
         ),
         (
             305,
@@ -449,6 +525,12 @@ fn md_limit_test() {
                     "Hi!",
                 ),
             ),
+            SoftBreak,
+            Text(
+                Borrowed(
+                    "你好！这里是中文！",
+                ),
+            ),
             End(
                 Paragraph,
             ),
@@ -543,17 +625,25 @@ fn md_limit_test() {
                 BlockQuote,
             ),
         ],
-        buffer: "I/O event queue\nWe add the `callback_id` to the collection of callbacks to run. We pass in `Js::Undefined` since we'll not actually pass any data along here. You'll see why when we reach the Http module chapter, but the main point is that the I/O queue doesn't return any data itself, it just tells us that data is ready to be read.\nHi!\nHi! Why even keep track of how many `epoll_events` are pending? We don't use this value here, but I added it to make it easier to create some `print` statements showing the status of our runtime at different points. However, there are good reasons to keep track of these events even if we don't use them.\nOne area we're taking shortcuts on all the way here is security. If someone were to build a public facing server out of this, we need to account for slow networks and malicious users.\n",
-        para: [
+        buffer: "I/O event queue\nWe add the `callback_id` to the collection of callbacks to run. We pass in `Js::Undefined` since we'll not actually pass any data along here. You'll see why when we reach the Http module chapter, but the main point is that the I/O queue doesn't return any data itself, it just tells us that data is ready to be read.\nHi! 你好！这里是中文！\nHi! Why even keep track of how many `epoll_events` are pending? We don't use this value here, but I added it to make it easier to create some `print` statements showing the status of our runtime at different points. However, there are good reasons to keep track of these events even if we don't use them.\nOne area we're taking shortcuts on all the way here is security. If someone were to build a public facing server out of this, we need to account for slow networks and malicious users.\n",
+        bytes: [
             16,
             317,
-            4,
+            32,
+            305,
+            184,
+        ],
+        chars: [
+            16,
+            317,
+            14,
             305,
             184,
         ],
         limit: Limit {
             limit: 0,
-            bat: 0,
+            cnt: 0,
+            len: 0,
             pos: 0,
         },
     }
@@ -592,8 +682,9 @@ fn md_split_append() {
     ```
 
     Hi!
+    你好！这里是中文！
 
-    Hi!
+    Hi! 你好！这里是中文！
 
      > 
      > Hi!
