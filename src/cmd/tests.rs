9ad6ae60555a5d128a1581d820f1977066e82281
zjp <jiping_zhou@foxmail.com>
2023-02-02 13:17:40 +0800

    doc

--- src/cmd/tests.rs
+++ src/cmd/tests.rs
@@ -1,11 +1,13 @@
 use super::*;
 use insta::{assert_debug_snapshot as snap, assert_display_snapshot as shot};
-use log::{Metadata, Record};
 use std::sync::Mutex;
 
+// 存储以测试日志信息（为了明确在出错的情况下会产生什么记录）
+// TODO: 如果需要测试更多日志，可公开此结构
 struct Logger(Mutex<String>);
 
 impl Logger {
+    // 初始化：此函数已经设置了开启日志，无需再设置；已设置级别为 Debug
     fn init() -> &'static Self {
         let logger = Box::leak(Box::new(Logger(Mutex::new(String::with_capacity(1024)))));
         log::set_logger(logger).expect("日志初始化失败：已经设置了日志");
@@ -13,22 +15,25 @@ impl Logger {
         logger
     }
 
-    fn take_content(&self) -> String { std::mem::take(&mut self.0.lock().unwrap()) }
+    // 获取存储的原日志（注意新的日志为空）
+    fn take_content(&self) -> String {
+        std::mem::replace(&mut *self.0.lock().unwrap(), String::with_capacity(1024))
+    }
 }
 
 impl log::Log for Logger {
-    fn enabled(&self, _metadata: &Metadata) -> bool { true }
+    fn enabled(&self, _metadata: &log::Metadata) -> bool { true }
 
-    fn log(&self, record: &Record) {
+    fn log(&self, record: &log::Record) {
         use std::fmt::Write;
-        let s = &mut *self.0.lock().unwrap();
-        writeln!(s, "LEVEL={}: {}", record.level(), record.args()).unwrap();
+        writeln!(&mut *self.0.lock().unwrap(), "LEVEL={}: {}", record.level(), record.args()).unwrap();
     }
 
     fn flush(&self) {}
 }
 
 #[test]
+// 模拟无配置文件下，id 和 key 设置时返回的错误与日志记录
 fn error_report_on_empty_config() {
     let logger = Logger::init();
 
